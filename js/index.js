c8974d061312d06badc77c07b64acd3daa76f203
thallysmaia16 <115167123+thallysmaia16@users.noreply.github.com>
2022-12-16 10:29:37 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
 const express = require('express')
-
+const path = require('path')
 const server = express()
 
-server.get('login', () =>{
-    console.log('Você está acessando a pagina de login')
+server.get('login', (req, res) =>{
+    res.sendFile(path.join(__dirname+'/../login.html'))
 })
 
-server.get('/index', () =>{
-    console.log('Você está acessando a pagina inicial')
+server.get('/index', (req, res) =>{
+    res.sendFile(path.join(__dirname+'/../index.html'))
+})
+
+server.get('/register', (req, res) =>{
+    res.sendFile(path.join(__dirname+'/../register.html'))
 })
 
 server.listen(3000)
